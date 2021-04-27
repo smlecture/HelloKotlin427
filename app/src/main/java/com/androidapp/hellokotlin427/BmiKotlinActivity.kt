0e0342a2ab402cd8bb2980dd72ca55c73b1fd941
H-J Yoo <yoohj@smu.ac.kr>
2021-04-27 17:22:38 +0900

    BMI calculation by Java & Kotlin

--- app/src/main/java/com/androidapp/hellokotlin427/BmiKotlinActivity.kt
+++ app/src/main/java/com/androidapp/hellokotlin427/BmiKotlinActivity.kt
@@ -18,14 +18,14 @@ class BmiKotlinActivity : AppCompatActivity() {
         val txtResult = findViewById<TextView>(R.id.txtResult)
 
         btnBmi.setOnClickListener {
-            try {
+            try{
                 val height = heightField.getText().toString().toDouble()
                 val weight = weightField.getText().toString().toDouble()
 
                 val bmi = weight / Math.pow(height / 100.0, 2.0)
 
                 txtResult.setText("Your BMI = " + bmi)
-            } catch (e : Exception){
+            }catch (e : Exception){
                 txtResult.setText("Input(s) missing")
             }
         }
